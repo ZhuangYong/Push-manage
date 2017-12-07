868ef471a0c42d6373661ceefe23f4b93bc54017
xl@huo.so <xl@huo.so>
2017-12-07 18:05:41 +0800

    配置管理用BaseListView统一

--- src/views/system/config.js
+++ src/views/system/config.js
@@ -1,5 +1,5 @@
-import {mapGetters} from "vuex";
-import BaseListView from '../../components/common/BaseListView1';
+import {mapGetters} from 'vuex';
+import BaseListView from '../../components/common/BaseListView';
 import {save as configSave} from '../../api/config';
 
 const defaultFormData = {
@@ -35,77 +35,74 @@ export default BaseListView.extend({
                 return this.system.configManage;
             },
             pageAction: 'config/RefreshPage',
-            defaultFormData: defaultFormData, // 默认表单值
+            defaultFormData: defaultFormData,
             tableCanSelect: false, // 表单项是否可以选择
-            formData: {} // 表单值
+            formData: {}
         };
     },
-
     computed: {
         ...mapGetters(['system'])
     },
     methods: {
 
-        /**
+       /**
          * 新增、修改、查看页面模板
          * @param h
          * @returns {XML}
-         */
+        */
         cruHtml: function (h) {
+          return (
+              <el-form v-loading={this.loading} class="small-space" model={this.formData}
+                       ref="addForm" rules={this.validateRule} label-position="right" label-width="180px">
+                  <el-form-item label="配置名称" prop="confName">
+                      <el-input value={this.formData.confName} name="confName"/>
+                  </el-form-item>
+                  <el-form-item label="配置值" prop="confValue">
+                      <el-input value={this.formData.confValue} name="confValue"/>
+                  </el-form-item>
+                  <el-form-item label="类型" prop="type">
+                      <el-select placeholder="请选择" value={this.formData.type} name='type'>
+                          <el-option
+                              value={1}
+                              label="系统配置"
+                              key={1}>
+                          </el-option>
+                          <el-option
+                              value={2}
+                              label="会员配置"
+                              key={2}>
+                          </el-option>
+                          <el-option
+                              value={3}
+                              label="支付配置"
+                              key={3}>
+                          </el-option>
+                      </el-select>
+                  </el-form-item>
+                  <el-form-item>
+                      <el-button type="primary" onClick={this.submitAddOrUpdate}>提交</el-button>
+                      <el-button onClick={
+                          () => {
+                              this.status = "list";
+                          }
+                      }>取消
+                      </el-button>
+                  </el-form-item>
+              </el-form>
+          );
+       },
+        topButtonHtml: function (h) {
             return (
-                <el-form v-loading={this.loading} class="small-space" model={this.formData}
-                         ref="addForm" rules={this.validateRule} label-position="right" label-width="180px">
-                    <el-form-item label="配置名称" prop="confName">
-                        <el-input value={this.formData.confName} name="confName"/>
-                    </el-form-item>
-                    <el-form-item label="配置值" prop="confValue">
-                        <el-input value={this.formData.confValue} name="confValue"/>
-                    </el-form-item>
-                    <el-form-item label="类型" prop="type">
-                        <el-select placeholder="请选择" value={this.formData.type} name='type'>
-                            <el-option
-                                value={1}
-                                label="系统配置"
-                                key={1}>
-                            </el-option>
-                            <el-option
-                                value={2}
-                                label="会员配置"
-                                key={2}>
-                            </el-option>
-                            <el-option
-                                value={3}
-                                label="支付配置"
-                                key={3}>
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" onClick={this.submitAddOrUpdate}>提交</el-button>
-                        <el-button onClick={
-                            () => {
-                                this.status = "list";
-                            }
-                        }>取消
-                        </el-button>
-                    </el-form-item>
-                </el-form>
-            );
-        },
-        filterHtml: function(h) {
-            return (
-                <el-form model={this.filters} inline ref="filterData">
-                    <el-form-item>
-                        <el-button class="filter-item" onClick={
-                            () => {
-                                this.status = "add";
-                                this.formData = Object.assign({}, this.defaultFormData);
-                                this.owned = [];
-                            }
-                        } type="primary" icon="edit">添加
-                        </el-button>
-                    </el-form-item>
-                </el-form>
+                this.status === "list" ? <div class="filter-container">
+                    <el-button class="filter-item" onClick={
+                        () => {
+                            this.status = "add";
+                            this.formData = Object.assign({}, this.defaultFormData);
+                            this.owned = [];
+                        }
+                    } type="primary" icon="edit">添加
+                    </el-button>
+                </div> : ""
             );
         },
         submitAddOrUpdate: function () {
