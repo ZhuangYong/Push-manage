b74f85fd8ba9c8f3b8a61738c28ca75b91001a20
walljack <walljack@163.com>
2017-12-22 09:51:29 +0800

    修改fix接口地址

--- config/project.config.fix.js
+++ config/project.config.fix.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"sit"',
-    BASE_API: '"http://120.27.250.104:9010/admin/"',
+    BASE_API: '"http://120.27.250.104:9010"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
